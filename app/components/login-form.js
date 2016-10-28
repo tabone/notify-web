a457a3b6cc7e927be738e04d876d55aa39446f29
Luca Tabone <taboneluca3@gmail.com>
2016-10-28 13:46:10 +0200

    Add a temporarily impl for Login

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,7 +1,49 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  /**
+   * username is binded with the Username Input Field.
+   * @type {String}
+   */
+  username: null,
+
+  /**
+   * password is binded with the Password Input Field.
+   * @type {String}
+   */
+  password: null,
+
+  /**
+   * session will be used to authenticate the user with the provided
+   * credentials.
+   * @type {service:session}
+   */
+  session: Ember.inject.service(),
+
+  /**
+   * routing is used to redirect the user to the app on successful
+   * authentication.
+   * @type {service:-routing}
+   */
+  routing: Ember.inject.service('-routing'),
+
+  /**
+   * didInsertElement is called when the element of the view has been inserted
+   * into the DOM or after the view was re-rendered.
+   */
   didInsertElement () {
     componentHandler.upgradeElements(this.$('[class*="mdl-js-"]'));
+  },
+
+  actions: {
+    /**
+     * login is used to authenticate the user with the crentials provided.
+     */
+    login () {
+      const username = this.get('username')
+      const password = this.get('password')
+      this.get('session').login(username, password)
+      this.get('routing').transitionTo('index')
+    }
   }
 });
