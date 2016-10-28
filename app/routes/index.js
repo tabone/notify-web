a91968c9e4d7fac4a08a3b033fdb19d6ca1aca4d
Luca Tabone <taboneluca3@gmail.com>
2016-10-28 14:14:56 +0200

    Redirect unauth user to login page when trying to access app page.
    When an unauthorized user tries to access the app, he should be
    redirected to the login page.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  /**
+   * session service is used to manage the session info.
+   * @type {service:session}
+   */
+  session: Ember.inject.service(),
+
+  beforeModel () {
+    // If the user is trying to access the app while not authenticated, he
+    // should be redirected to the login page.
+    if (this.get('session').isLoggedIn() === false) this.transitionTo('login')
+  }
 });
