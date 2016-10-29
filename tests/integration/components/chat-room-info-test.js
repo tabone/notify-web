c345ae9734d4f33d035cf525b114440a39552a65
Luca Tabone <taboneluca3@gmail.com>
2016-10-29 20:27:34 +0200

    Start the work on Room Info
    The Room Info section will be the place where the user will be
    able to view info about the Room he is currently in, like:
      * Room name.
      * Room members.
      * Room actions.

---
+++ tests/integration/components/chat-room-info-test.js
@@ -0,0 +1,24 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('chat-room-info', 'Integration | Component | chat room info', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{chat-room-info}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#chat-room-info}}
+      template block text
+    {{/chat-room-info}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
