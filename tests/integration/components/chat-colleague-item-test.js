52f52895e9b43f7368a480fa6334e556a8b8a491
Luca Tabone <taboneluca3@gmail.com>
2016-10-29 15:10:21 +0200

    Fix typo in component name

--- tests/integration/components/chat-colleague-item-test.js
+++ tests/integration/components/chat-colleague-item-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('chat-collegue-item', 'Integration | Component | chat collegue item', {
+moduleForComponent('chat-colleague-item', 'Integration | Component | chat colleague item', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{chat-collegue-item}}`);
+  this.render(hbs`{{chat-colleague-item}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#chat-collegue-item}}
+    {{#chat-colleague-item}}
       template block text
-    {{/chat-collegue-item}}
+    {{/chat-colleague-item}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
