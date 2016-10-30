be93b9cf5dacb86311389ac1e0f75260731db1c3
Luca Tabone <taboneluca3@gmail.com>
2016-10-30 17:37:24 +0100

    Move the sidebar panels in their own component
    In an attempt to make the app more modular, this commit moves the
    tab panels witin the sidebar section (showing the list of users and
    list of rooms) in their own component.

---
+++ tests/integration/components/chat-sidebar-rooms-list-test.js
@@ -0,0 +1,24 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('chat-sidebar-rooms-list', 'Integration | Component | chat sidebar rooms list', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{chat-sidebar-rooms-list}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#chat-sidebar-rooms-list}}
+      template block text
+    {{/chat-sidebar-rooms-list}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
