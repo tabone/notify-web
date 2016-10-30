7096526677b373b579934ec88d3c9ba0d81a0710
Luca Tabone <taboneluca3@gmail.com>
2016-10-30 21:46:30 +0100

    Move /index route logic to a new /app route

--- app/routes/login.js
+++ app/routes/login.js
@@ -10,6 +10,6 @@ export default Ember.Route.extend({
   beforeModel () {
     // If the user is trying to access the login page while already
     // authenticated, he should be redirected to the app.
-    if (this.get('session').isLoggedIn() === true) this.transitionTo('index')
+    if (this.get('session').isLoggedIn() === true) this.transitionTo('app')
   }
 });
