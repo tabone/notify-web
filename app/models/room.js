393c522c76af45297e5b2ab6cc5981039ea2a504
Luca Tabone <taboneluca3@gmail.com>
2016-11-02 22:38:06 +0100

    Move logic used to get the room name and image in the room model

--- app/models/room.js
+++ app/models/room.js
@@ -1,6 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  session: Ember.inject.service(),
+
   /**
    * name of the room.
    */
@@ -23,5 +25,50 @@ export default DS.Model.extend({
    */
   messages: DS.hasMany('message', {
     inverse: 'room'
+  }),
+
+  /**
+   * uiName is the room name used in the UI.
+   */
+  uiName: Ember.computed('name', function () {
+    let name = this.get('name')
+
+    // If the room has been assigned a name, use the assigned name.
+    if (name !== null) return name
+
+    // In case the room hasn't got a name, the app should concatinate the
+    // usernames of the members (excluding the logged in user) and use it as the
+    // name for the room.
+    const session = this.get('session')
+    name = []
+
+    const users = this.get('users')
+
+    users.forEach(user => {
+      if (user === session.get('user')) return
+      name.push(user.get('username'))
+    })
+
+    return name.join(', ')
+  }),
+
+  /**
+   * uiImage is the image name used in the UI.
+   */
+  uiImage: Ember.computed('image', function () {
+    let image = this.get('image')
+
+    // If the room has been assigned an image, use the assigned image.
+    if (image !== null) return image
+
+    // In case the room hasn't got an image, the app should use a random image
+    // of a member (excluding the logged in user).
+    const session = this.get('session')
+    const users = this.get('users')
+
+    let userIndex = 0
+    if (users.objectAt(userIndex) === session.get('user')) userIndex++
+    console.log(users.objectAt(userIndex).get('image'))
+    return users.objectAt(userIndex).get('image')
   })
 });
