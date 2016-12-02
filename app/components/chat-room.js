6431159299c016d9d5c2560b3868b3b46240d618
Luca Tabone <taboneluca3@gmail.com>
2016-12-02 20:19:12 +0100

    Include comments

--- app/components/chat-room.js
+++ app/components/chat-room.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  /**
+   * didInsertElement is called when the element of the view has been inserted
+   * into the DOM or after the view was re-rendered.
+   */
   didInsertElement () {
     this.$('.app-chat-room')[0].style.height = window.innerHeight + 'px'
   }
