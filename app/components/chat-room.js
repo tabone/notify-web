633e76a7c6d248f20f30eff42e214f02c98a05c8
Luca Tabone <taboneluca3@gmail.com>
2016-12-03 21:15:50 +0100

    Improve full height listener functionality

--- app/components/chat-room.js
+++ app/components/chat-room.js
@@ -2,11 +2,35 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['app-chat-room'],
+
+  /**
+   * listener used to keep the chat room component at full height.
+   * @type {Function}
+   */
+  listener: null,
+
   /**
    * didInsertElement is called when the element of the view has been inserted
    * into the DOM or after the view was re-rendered.
    */
   didInsertElement () {
-    this.$()[0].style.height = window.innerHeight + 'px'
+    // The root element of the chat room component.
+    const rootElem = this.$()[0]
+
+    const fn = () => {
+      rootElem.style.height = window.innerHeight + 'px'
+    }
+    
+    fn()
+    this.set('listener', fn)
+    window.addEventListener('resize', fn)
+  },
+
+  /**
+   * willDestroyElement is called before the element of the view is removed by
+   * Ember.
+   */
+  willDestroyElement () {
+    window.removeEventListener('resize', this.get('listener'))
   }
 });
