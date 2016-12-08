819fe29042190f53c54342c90ca011b461115ab2
Luca Tabone <taboneluca3@gmail.com>
2016-12-08 12:36:18 +0100

    Add functionality to show the invitation dialog

--- app/components/chat-room-info.js
+++ app/components/chat-room-info.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['chat-room-info']
+  classNames: ['chat-room-info'],
+  actions: {
+    /**
+     * showInviteDialog shows the invitation dialog.
+     */
+    showInviteDialog () {
+      this.$('dialog')[0].showModal()
+    }
+  }
 });
