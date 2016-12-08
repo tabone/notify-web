c27daec85ef472eda8504fb2edcd7ac7db2f6748
Luca Tabone <taboneluca3@gmail.com>
2016-12-08 18:37:51 +0100

    Include comments

--- app/components/chat-colleague-item.js
+++ app/components/chat-colleague-item.js
@@ -1,8 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  /**
+   * privateRoomCache is used to cache private rooms by the friend id.
+   * @type {service:private-room-cache}
+   */
   privateRoomCache: Ember.inject.service(),
 
+  /**
+   * room is the private room of the user being displayed and the logged in
+   * user.
+   * @type {[type]}
+   */
   room: null,
 
   init (...args) {
