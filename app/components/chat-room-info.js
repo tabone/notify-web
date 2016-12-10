1811d8753c343b8b92c748886dec6423b59e36e4
Luca Tabone <taboneluca3@gmail.com>
2016-12-10 12:16:49 +0100

    Include func to leave non-private rooms

--- app/components/chat-room-info.js
+++ app/components/chat-room-info.js
@@ -2,12 +2,31 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['chat-room-info'],
+
+  /**
+   * session is used to get the details of the current logged in user.
+   * @type {service:session}
+   */
+  session: Ember.inject.service(),
+
   actions: {
     /**
      * showInviteDialog shows the invitation dialog.
      */
     showInviteDialog () {
       this.$('dialog')[0].showModal()
+    },
+
+    /**
+     * leaveRoom removes the logged in user from the room he is currently in.
+     * @return {Promise} Resolved once the change has been persisted.
+     */
+    leaveRoom () {
+      const room = this.get('room')
+      room.get('users').removeObject(this.get('session.user'))
+      return room.save()
+        .then(() => room.unloadRecord())
+        .then(() => this.get('router').transitionTo('chat'))
     }
   }
 });
