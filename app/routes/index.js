e5dcc95db647f6bccfa06186d9eabdf9d502fa99
Luca Tabone <taboneluca3@gmail.com>
2017-01-15 17:10:04 +0100

    Redirect user to login page if he is not logged

--- app/routes/index.js
+++ app/routes/index.js
@@ -10,6 +10,10 @@ export default Ember.Route.extend({
   beforeModel () {
     // If the user is trying to access the login page while already
     // authenticated, he should be redirected to the chat.
-    if (this.get('session').isLoggedIn() === true) this.transitionTo('chat')
+    if (this.get('session').isLoggedIn() === true) {
+      this.transitionTo('chat')
+    } else {
+      window.location = '/login'
+    }
   }
 });
