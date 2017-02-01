6cec3723732a30ce18291f6a563a98003138973f
Luca Tabone <taboneluca3@gmail.com>
2017-02-01 20:35:01 +0100

    Include comments

--- app/components/app-notification.js
+++ app/components/app-notification.js
@@ -1,6 +1,14 @@
 import Ember from 'ember';
 
+/**
+ * Component used to display notifications. These notifications are displayed
+ * through the app's Notification service.
+ */
 export default Ember.Component.extend({
+  /**
+   * classNames to be added to the root element of the component.
+   * @type {Array}
+   */
   classNames: ['app-notification', 'mdl-js-snackbar', 'mdl-snackbar'],
 
   /**
@@ -21,17 +29,17 @@ export default Ember.Component.extend({
    * into the DOM or after the view was re-rendered.
    */
   didInsertElement () {
-    componentHandler.upgradeElements(this.$()[0]);
+    componentHandler.upgradeElements(this.$()[0])
     this.set('elem', this.$()[0])
-    this.get('notification').addListener(this.onnotification.bind(this))
+    this.get('notification').addListener(this.onNotification.bind(this))
   },
 
   /**
-   * onnotification is triggered when there is a notification to be displayed.
+   * onNotification is triggered when there is a notification to be displayed.
    * @param  {String} payload.type    The type of the message.
    * @param  {String} payload.message The message to display.
    */
-  onnotification (payload) {
+  onNotification (payload) {
     this.get('elem').MaterialSnackbar.showSnackbar({
       message: payload.message,
       timeout: 10000,
