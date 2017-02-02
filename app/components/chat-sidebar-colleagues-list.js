8e613524c77e2a719ba1ebbc0a73954a2b69cf43
Luca Tabone <taboneluca3@gmail.com>
2017-02-02 19:59:54 +0100

    List only valid friends
    Currently the users with whome the logged in user was able to chat
    with (a.k.a Colleague's List) also listed the user himself. This
    commit changes this.
    
    Note that the logic to not list bots was moved from the template to
    the JS file since it wasn't possible to do multiple checks.

--- app/components/chat-sidebar-colleagues-list.js
+++ app/components/chat-sidebar-colleagues-list.js
@@ -24,5 +24,31 @@ export default Ember.Component.extend({
    * ariaLabel describing the component.
    * @type {String}
    */
-  ariaLabel: 'List of Collegues'
+  ariaLabel: 'List of Collegues',
+
+  /**
+   * session is used to get the details of the current logged in user.
+   * @type {service:session}
+   */
+  session: Ember.inject.service(),
+
+  /**
+   * friends which the user can chat with. This list should contain all non-bot
+   * users expect the logged in user himself..
+   * @type {Array{Records}}
+   */
+  friends: null,
+
+  /**
+   * didRecieveAttrs runs after init hook, and it also runs on subsequent
+   * re-renders.
+   */
+  didReceiveAttrs () {
+    // Filter friends.
+    const friends = this.get('users').filter(user => {
+      return (user.get('bot') !== true) && (user !== this.get('session.user'))
+    })
+
+    this.set('friends', friends)
+  }
 });
