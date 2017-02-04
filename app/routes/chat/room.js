f87c51f7997e8555c6679f190eb88fe422631c6e
Luca Tabone <taboneluca3@gmail.com>
2017-02-04 09:14:51 +0100

    Redirect user to chat if room id is null
    Currently when the user refreshes the page while having a non
    persisted private room, the page was not loading since it tried to
    retrieve a room with an id set to 'null'. This commit fixes this.

--- app/routes/chat/room.js
+++ app/routes/chat/room.js
@@ -4,7 +4,11 @@ export default Ember.Route.extend({
   /**
    * model hook is used to convert the url to a model.
    */
-  model (params) {
+  model (params, transition) {
+    // If the room id is set to null, redirect the user to the chat page. This
+    // happens when the user refreshes the page while having a new private room
+    // opened.
+    if (params['room_id'] === 'null') return this.transitionTo('chat')
     return this.get('store').findRecord('room', params['room_id'])
   }
 });
