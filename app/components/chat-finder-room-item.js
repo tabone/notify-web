6b863146f65624a8f51d29ec879bc319b6092180
Luca Tabone <taboneluca3@gmail.com>
2017-03-07 19:50:34 +0100

    Add component to represent Room Finder Item
    The Room Finder Item component will be used to represent Room items
    inside the Finder dialog. When clicked upon (or clicking on the
    enter key while focusing this component) it will redirect the user
    to the room the component is representing.

---
+++ app/components/chat-finder-room-item.js
@@ -0,0 +1,46 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  /**
+   * tagName of the root element.
+   * @type {String}
+   */
+  tagName: 'li',
+
+  /**
+   * classNames to be added to the root element of the component.
+   * @type {Array}
+   */
+  classNames: ['mdl-list__item'],
+
+  /**
+   * attributeBindings binds the instance fields with the root element
+   * attributes.
+   * @type {Array}
+   */
+  attributeBindings: ['tabindex'],
+
+  /**
+   * tabIndex of the component.
+   * @type {String}
+   */
+  tabindex: '0',
+
+  /**
+   * click is invoked when the user clicks on the component. When invoked it
+   * redirects the user to the room this component is representing.
+   */
+  click () {
+    this.get('router').transitionTo('chat.room', this.get('data'))
+  },
+
+  /**
+   * keyDown is invoked when the user presses a key while focusing the
+   * component. When invoked it redirects the user to the room this component
+   * is representing
+   * @param  {Object} ev KeyDown event object
+   */
+  keyDown (ev) {
+    this.click()
+  }
+})
---
+++ tests/integration/components/chat-finder-room-item-test.js
@@ -0,0 +1,25 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('chat-finder-room-item', 'Integration | Component | chat finder room item', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{chat-finder-room-item}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#chat-finder-room-item}}
+      template block text
+    {{/chat-finder-room-item}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
