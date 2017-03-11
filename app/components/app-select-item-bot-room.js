ca76986ea2df85a6689419ccbb3f612391c315c1
Luca Tabone <taboneluca3@gmail.com>
2017-03-11 19:09:51 +0100

    Implement app-select item component for bot rooms

--- app/components/app-select-item-bot-room.js
+++ app/components/app-select-item-bot-room.js
@@ -1,4 +1,29 @@
-import Ember from 'ember';
+import Ember from 'ember'
 
+/**
+ * This component is used by app-select component to represent an item which the
+ * user can select. In this case it will represent rooms which a bot can write
+ * in.
+ * @param {Object}   item    The room that can be selected.
+ * @param {Function} select  Function used to select the room.
+ */
 export default Ember.Component.extend({
-});
+  /**
+   * tagName of the root element.
+   * @type {String}
+   */
+  tagName: ['li'],
+
+  /**
+   * classNames to be added to the root element of the component.
+   * @type {Array}
+   */
+  classNames: ['app-select-item-bot-room', 'mdl-list__item'],
+
+  /**
+   * click is invoked when the user clicks on the component.
+   */
+  click () {
+    this.get('select')(this.get('item'))
+  }
+})
