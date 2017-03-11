4b6b94aae27578308288316190136e9afc5ad55c
Luca Tabone <taboneluca3@gmail.com>
2017-03-11 12:04:39 +0100

    Implement app-select selected items component for invite dialog

--- app/components/app-select-chip-invite-user.js
+++ app/components/app-select-chip-invite-user.js
@@ -1,4 +1,31 @@
-import Ember from 'ember';
+import Ember from 'ember'
 
+/**
+ * This component is used by app-select component to represent selected items.
+ * In this case this component will represent users who were selected to get
+ * invited.
+ * @param {Object}   item      The selected user.
+ * @param {Function} unselect  Function used to unselect the user.
+ */
 export default Ember.Component.extend({
-});
+  /**
+   * tagName of the root element.
+   * @type {String}
+   */
+  tagName: 'span',
+
+  /**
+   * classNames to be added to the root element of the component.
+   * @type {Array}
+   */
+  classNames: ['mdl-chip', 'mdl-chip--deletable'],
+
+  actions: {
+    /**
+     * unselect the user that this component is representing.
+     */
+    unselect () {
+      this.get('unselect')(this.get('item'))
+    }
+  }
+})
