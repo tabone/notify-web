cf432e5ce8021f12e3deae5a378d7ab32a0b6d76
Luca Tabone <taboneluca3@gmail.com>
2017-03-11 19:06:41 +0100

    Implement app-select chip component for bot rooms

--- app/components/app-select-chip-bot-room.js
+++ app/components/app-select-chip-bot-room.js
@@ -1,4 +1,30 @@
-import Ember from 'ember';
+import Ember from 'ember'
 
+/**
+ * This component is used by app-select component to represent selected items.
+ * In this case this component will represent rooms which the bot can write in.
+ * @param {Object}   item      The selected room.
+ * @param {Function} unselect  Function used to unselect the room.
+ */
 export default Ember.Component.extend({
-});
+  /**
+   * tagName of the root element.
+   * @type {String}
+   */
+  tagName: 'span',
+
+  /**
+   * classNames to be added to the root element of the component.
+   * @type {Array}
+   */
+  classNames: ['mdl-chip', 'mdl-chip--deletable'],
+
+  actions: {
+    /**
+     * unselect the user that this component is representing.
+     */
+    unselect () {
+      this.get('unselect')(this.get('item'))
+    }
+  }
+})
