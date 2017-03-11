3441819e4c9f67fcfc4ec66e6115d44ee03487e1
Luca Tabone <taboneluca3@gmail.com>
2017-03-11 19:43:32 +0100

    Make user items accessible via keyboard

--- app/components/app-select-item-invite-user.js
+++ app/components/app-select-item-invite-user.js
@@ -20,10 +20,33 @@ export default Ember.Component.extend({
   classNames: ['app-select-item-invite-user', 'mdl-list__item'],
 
   /**
+   * attributeBindings binds the instance fields with the root element
+   * attributes.
+   * @type {Array}
+   */
+  attributeBindings: ['tabindex'],
+
+  /**
+   * tabIndex of the component.
+   * @type {String}
+   */
+  tabindex: '0',
+
+  /**
    * click is invoked when the user clicks on the component. When clicked the
    * user who is being represented by this component is selected to get invited.
    */
   click () {
     this.get('select')(this.get('item'))
+  },
+
+  /**
+   * keyDown is invoked when the user presses a key while focusing on the
+   * component. When triggered with the enter key it should be considered as a
+   * click.
+   * @param  {Object} ev  KeyDown event.
+   */
+  keyDown (ev) {
+    if (ev.keyCode === 13) this.click()
   }
 })
