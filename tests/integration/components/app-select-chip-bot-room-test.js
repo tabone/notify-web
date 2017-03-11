97d18d5df0761185900a2620cb2ccc5748806e1b
Luca Tabone <taboneluca3@gmail.com>
2017-03-11 13:48:13 +0100

    Add new component app-select-chip-bot-room
    This component will be used by the `app-select` component to
    represent selected items (or in this case the rooms which a bot is
    permitted to write in).

---
+++ tests/integration/components/app-select-chip-bot-room-test.js
@@ -0,0 +1,25 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('app-select-chip-bot-room', 'Integration | Component | app select chip bot room', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{app-select-chip-bot-room}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#app-select-chip-bot-room}}
+      template block text
+    {{/app-select-chip-bot-room}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
