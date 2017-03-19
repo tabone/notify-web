d50e10588885fd16a385dc40008001569b0190a9
Luca Tabone <taboneluca3@gmail.com>
2017-03-19 20:18:34 +0100

    Lint code

--- tests/integration/components/app-select-item-bot-room-test.js
+++ tests/integration/components/app-select-item-bot-room-test.js
@@ -1,25 +1,24 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from 'ember-qunit'
+import hbs from 'htmlbars-inline-precompile'
 
 moduleForComponent('app-select-item-bot-room', 'Integration | Component | app select item bot room', {
   integration: true
-});
+})
 
-test('it renders', function(assert) {
+test('it renders', function (assert) {
+  // Set any properties with this.set('myProperty', 'value')
+  // Handle any actions with this.on('myAction', function(val) { ... })
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  this.render(hbs`{{app-select-item-bot-room}}`)
 
-  this.render(hbs`{{app-select-item-bot-room}}`);
-
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$().text().trim(), '')
 
   // Template block usage:
   this.render(hbs`
     {{#app-select-item-bot-room}}
       template block text
     {{/app-select-item-bot-room}}
-  `);
+  `)
 
-  assert.equal(this.$().text().trim(), 'template block text');
-});
+  assert.equal(this.$().text().trim(), 'template block text')
+})
--- tests/integration/components/chat-finder-dialog-test.js
+++ tests/integration/components/chat-finder-dialog-test.js
@@ -1,25 +1,24 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from 'ember-qunit'
+import hbs from 'htmlbars-inline-precompile'
 
 moduleForComponent('chat-finder-dialog', 'Integration | Component | chat finder dialog', {
   integration: true
-});
+})
 
-test('it renders', function(assert) {
+test('it renders', function (assert) {
+  // Set any properties with this.set('myProperty', 'value')
+  // Handle any actions with this.on('myAction', function(val) { ... })
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  this.render(hbs`{{chat-finder-dialog}}`)
 
-  this.render(hbs`{{chat-finder-dialog}}`);
-
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$().text().trim(), '')
 
   // Template block usage:
   this.render(hbs`
     {{#chat-finder-dialog}}
       template block text
     {{/chat-finder-dialog}}
-  `);
+  `)
 
-  assert.equal(this.$().text().trim(), 'template block text');
-});
+  assert.equal(this.$().text().trim(), 'template block text')
+})
