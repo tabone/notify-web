3373d8eb9d8df888ed410134043b7834213390ad
Luca Tabone <taboneluca3@gmail.com>
2017-03-23 21:49:08 +0100

    Add tests for Token Model

--- tests/unit/models/token-test.js
+++ tests/unit/models/token-test.js
@@ -2,7 +2,7 @@ import { moduleForModel, test } from 'ember-qunit'
 
 moduleForModel('token', 'Unit | Model | token', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:user']
 })
 
 test('it exists', function (assert) {
