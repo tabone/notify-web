688b7d42aa3526f2c682e6b03c66f763a731434b
Luca Tabone <taboneluca3@gmail.com>
2017-03-23 21:45:01 +0100

    Include tests for Message Model

--- tests/unit/models/message-test.js
+++ tests/unit/models/message-test.js
@@ -2,11 +2,103 @@ import { moduleForModel, test } from 'ember-qunit'
 
 moduleForModel('message', 'Unit | Model | message', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:user', 'model:room']
 })
 
-test('it exists', function (assert) {
-  let model = this.subject()
-  // let store = this.store()
-  assert.ok(!!model)
+test('ETA for message created less than 20sec ago', function (assert) {
+  let message = this.subject({ created: new Date() })
+  assert.equal(message.get('uiCreated'), 'just now')
+})
+
+test('ETA for message created between 20sec (incl) & 1min (excl) ago', function (assert) {
+  const eta = 25
+  const created = new Date()
+  created.setSeconds(created.getSeconds() - eta)
+  let message = this.subject({ created: created })
+  assert.equal(message.get('uiCreated'), `${eta} seconds ago`)
+})
+
+test('ETA for message created between 1min (incl) & 2min (excl) ago', function (assert) {
+  const created = new Date()
+  created.setSeconds(created.getSeconds() - 68)
+  let message = this.subject({ created: created })
+  assert.equal(message.get('uiCreated'), '1 minute ago')
+})
+
+test('ETA for message created between 1min (excl) & 1hr (excl) ago', function (assert) {
+  const created = new Date()
+  created.setSeconds(created.getSeconds() - 121)
+  let message = this.subject({ created: created })
+  assert.equal(message.get('uiCreated'), '2 minutes ago')
+})
+
+test('ETA for message created between 1hr (incl) & 2hr (excl) ago', function (assert) {
+  const created = new Date()
+  created.setMinutes(created.getMinutes() - 64)
+  let message = this.subject({ created: created })
+  assert.equal(message.get('uiCreated'), '1 hour ago')
+})
+
+test('ETA for message created between 1hr (excl) & 1 day (excl) ago', function (assert) {
+  const created = new Date()
+  created.setMinutes(created.getMinutes() - 126)
+  let message = this.subject({ created: created })
+  assert.equal(message.get('uiCreated'), '2 hours ago')
+})
+
+test('ETA for message created between 1 day (incl) & 2 days (excl) ago', function (assert) {
+  const created = new Date()
+  created.setHours(created.getHours() - 26)
+  let message = this.subject({ created: created })
+  assert.equal(message.get('uiCreated'), '1 day ago')
+})
+
+test('ETA for message created between 1 day (excl) & 1 week (excl) ago', function (assert) {
+  const created = new Date()
+  created.setHours(created.getHours() - 50)
+  let message = this.subject({ created: created })
+  assert.equal(message.get('uiCreated'), '2 days ago')
+})
+
+test('ETA for message created between 1 week (incl) & 2 weeks (excl) ago', function (assert) {
+  const created = new Date()
+  created.setDate(created.getDate() - 9)
+  let message = this.subject({ created: created })
+  assert.equal(message.get('uiCreated'), '1 week ago')
+})
+
+test('ETA for message created between 1 week (excl) & 1 month (excl) ago', function (assert) {
+  const created = new Date()
+  created.setDate(created.getDate() - 17)
+  let message = this.subject({ created: created })
+  assert.equal(message.get('uiCreated'), '2 weeks ago')
+})
+
+test('ETA for message created between 1 month (incl) & 2 months (excl) ago', function (assert) {
+  const created = new Date()
+  created.setDate(created.getDate() - (5 * 7))
+  let message = this.subject({ created: created })
+  assert.equal(message.get('uiCreated'), '1 month ago')
+})
+
+test('ETA for message created between 1 month (excl) & 1 year (excl) ago', function (assert) {
+  const created = new Date()
+  created.setDate(created.getDate() - (10 * 7))
+  let message = this.subject({ created: created })
+  assert.equal(message.get('uiCreated'), '2 months ago')
+})
+
+test('ETA for message created between 1 year (incl) & 2 year (excl) ago', function (assert) {
+  const created = new Date()
+  created.setMonth(created.getMonth() - 16)
+  let message = this.subject({ created: created })
+  assert.equal(message.get('uiCreated'), '1 year ago')
+})
+
+test('ETA for message created 2 year (incl) or more years ago', function (assert) {
+  const eta = 7
+  const created = new Date()
+  created.setFullYear(created.getFullYear() - eta)
+  let message = this.subject({ created: created })
+  assert.equal(message.get('uiCreated'), `${eta} years ago`)
 })
