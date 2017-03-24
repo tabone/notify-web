f9f0f3a447b878335e2f64f145144a2f62d02123
Luca Tabone <taboneluca3@gmail.com>
2017-03-24 23:49:15 +0100

    Include tests for User Model

--- tests/unit/models/user-test.js
+++ tests/unit/models/user-test.js
@@ -2,11 +2,22 @@ import { moduleForModel, test } from 'ember-qunit'
 
 moduleForModel('user', 'Unit | Model | user', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: [
+    'model:room',
+    'model:state',
+    'model:grant',
+    'model:message',
+    'model:token'
+  ]
 })
 
-test('it exists', function (assert) {
-  let model = this.subject()
-  // let store = this.store()
-  assert.ok(!!model)
+test('Image for a user who does not have a image', function (assert) {
+  let model = this.subject({ image: null })
+  assert.equal(model.get('uiImage'), 'no-user-image.png')
+})
+
+test('Image for a user who does have a image', function (assert) {
+  const userImage = 'user.png'
+  let model = this.subject({ image: userImage })
+  assert.equal(model.get('uiImage'), userImage)
 })
