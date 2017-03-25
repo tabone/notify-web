d1b6bf1ec93a6ba60be300d399448d8c25cf3121
Luca Tabone <tabone@users.noreply.github.com>
2017-03-25 17:21:07 +0100

    Add tests for JSONAPI adapter
    Add tests for JSONAPI adapter

--- tests/unit/adapters/application-test.js
+++ tests/unit/adapters/application-test.js
@@ -1,12 +1,28 @@
 import { moduleFor, test } from 'ember-qunit'
 
-moduleFor('adapter:application', 'Unit | Adapter | application', {
-  // Specify the other units that are required for this test.
-  // needs: ['serializer:foo']
+/**
+ * Stubbing the Config Service.
+ * @type {service}
+ */
+const configStub = Ember.Service.extend({
+  api: {
+    url: 'http://hikupz.io/api'
+  }
 })
 
-// Replace this with your real tests.
-test('it exists', function (assert) {
-  let adapter = this.subject()
-  assert.ok(adapter)
+moduleFor('adapter:application', 'Unit | Adapter | application', {
+  beforeEach: function beforeEach () {
+    this.register('service:config', configStub)
+    this.inject.service('config')
+  }
+})
+
+/**
+ * When initializing the JSON-API Adapter, its host should be the same as the
+ * host provided by the Config Service.
+ */
+test('When initialized it should set the host from the config service', function (assert) {
+  assert.expect(1)
+  const adapter = this.subject()
+  assert.strictEqual(adapter.get('host'), adapter.get('config.api.url'))
 })
