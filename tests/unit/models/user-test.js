6def183b405ca56035856ecde23626bf01f567f2
Luca Tabone <taboneluca3@gmail.com>
2017-03-25 16:11:24 +0100

    Fix test descriptions

--- tests/unit/models/user-test.js
+++ tests/unit/models/user-test.js
@@ -11,20 +11,20 @@ moduleForModel('user', 'Unit | Model | user', {
 })
 
 /**
- * When a user has been assigned an image, when display the user's image in the
- * app, it should display the assigned image.
+ * When a user has been assigned an image, when display the user's image, it
+ * should display the assigned image.
  */
-test('Image for a user who does not have a image', function (assert) {
+test('Image of a user who has not been assigned an image', function (assert) {
   assert.expect(1)
   const model = this.subject({ image: null })
   assert.strictEqual(model.get('uiImage'), 'no-user-image.png')
 })
 
 /**
- * When a user has not been assigned an image, when displaying he user's image
- * in the app, it should display the default app image.
+ * When a user has not been assigned an image, when displaying he user's image,
+ * it should display the default app image.
  */
-test('Image for a user who does have a image', function (assert) {
+test('Image of a user who has been assigned an image', function (assert) {
   assert.expect(1)
   const userImage = 'user.png'
   const model = this.subject({ image: userImage })
