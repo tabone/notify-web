7a162f4852054f1514adc1c7058f34f20ed50305
Luca Tabone <taboneluca3@gmail.com>
2017-03-25 09:40:49 +0100

    Assert using strict equality

--- tests/unit/models/message-test.js
+++ tests/unit/models/message-test.js
@@ -7,98 +7,98 @@ moduleForModel('message', 'Unit | Model | message', {
 
 test('Age for message created less than 20sec ago', function (assert) {
   let message = this.subject({ created: new Date() })
-  assert.equal(message.get('uiCreated'), 'just now')
+  assert.strictEqual(message.get('uiCreated'), 'just now')
 })
 
 test('Age for message created between 20sec (incl) & 1min (excl) ago', function (assert) {
-  const eta = 25
+  const seconds = 25
   const created = new Date()
-  created.setSeconds(created.getSeconds() - eta)
+  created.setSeconds(created.getSeconds() - seconds)
   let message = this.subject({ created: created })
-  assert.equal(message.get('uiCreated'), `${eta} seconds ago`)
+  assert.strictEqual(message.get('uiCreated'), `${seconds} seconds ago`)
 })
 
 test('Age for message created between 1min (incl) & 2min (excl) ago', function (assert) {
   const created = new Date()
   created.setSeconds(created.getSeconds() - 68)
   let message = this.subject({ created: created })
-  assert.equal(message.get('uiCreated'), '1 minute ago')
+  assert.strictEqual(message.get('uiCreated'), '1 minute ago')
 })
 
 test('Age for message created between 1min (excl) & 1hr (excl) ago', function (assert) {
   const created = new Date()
   created.setSeconds(created.getSeconds() - 121)
   let message = this.subject({ created: created })
-  assert.equal(message.get('uiCreated'), '2 minutes ago')
+  assert.strictEqual(message.get('uiCreated'), '2 minutes ago')
 })
 
 test('Age for message created between 1hr (incl) & 2hr (excl) ago', function (assert) {
   const created = new Date()
   created.setMinutes(created.getMinutes() - 64)
   let message = this.subject({ created: created })
-  assert.equal(message.get('uiCreated'), '1 hour ago')
+  assert.strictEqual(message.get('uiCreated'), '1 hour ago')
 })
 
 test('Age for message created between 1hr (excl) & 1 day (excl) ago', function (assert) {
   const created = new Date()
   created.setMinutes(created.getMinutes() - 126)
   let message = this.subject({ created: created })
-  assert.equal(message.get('uiCreated'), '2 hours ago')
+  assert.strictEqual(message.get('uiCreated'), '2 hours ago')
 })
 
 test('Age for message created between 1 day (incl) & 2 days (excl) ago', function (assert) {
   const created = new Date()
   created.setHours(created.getHours() - 26)
   let message = this.subject({ created: created })
-  assert.equal(message.get('uiCreated'), '1 day ago')
+  assert.strictEqual(message.get('uiCreated'), '1 day ago')
 })
 
 test('Age for message created between 1 day (excl) & 1 week (excl) ago', function (assert) {
   const created = new Date()
   created.setHours(created.getHours() - 50)
   let message = this.subject({ created: created })
-  assert.equal(message.get('uiCreated'), '2 days ago')
+  assert.strictEqual(message.get('uiCreated'), '2 days ago')
 })
 
 test('Age for message created between 1 week (incl) & 2 weeks (excl) ago', function (assert) {
   const created = new Date()
   created.setDate(created.getDate() - 9)
   let message = this.subject({ created: created })
-  assert.equal(message.get('uiCreated'), '1 week ago')
+  assert.strictEqual(message.get('uiCreated'), '1 week ago')
 })
 
 test('Age for message created between 1 week (excl) & 1 month (excl) ago', function (assert) {
   const created = new Date()
   created.setDate(created.getDate() - 17)
   let message = this.subject({ created: created })
-  assert.equal(message.get('uiCreated'), '2 weeks ago')
+  assert.strictEqual(message.get('uiCreated'), '2 weeks ago')
 })
 
 test('Age for message created between 1 month (incl) & 2 months (excl) ago', function (assert) {
   const created = new Date()
   created.setDate(created.getDate() - (5 * 7))
   let message = this.subject({ created: created })
-  assert.equal(message.get('uiCreated'), '1 month ago')
+  assert.strictEqual(message.get('uiCreated'), '1 month ago')
 })
 
 test('Age for message created between 1 month (excl) & 1 year (excl) ago', function (assert) {
   const created = new Date()
   created.setDate(created.getDate() - (10 * 7))
   let message = this.subject({ created: created })
-  assert.equal(message.get('uiCreated'), '2 months ago')
+  assert.strictEqual(message.get('uiCreated'), '2 months ago')
 })
 
 test('Age for message created between 1 year (incl) & 2 year (excl) ago', function (assert) {
   const created = new Date()
   created.setMonth(created.getMonth() - 16)
   let message = this.subject({ created: created })
-  assert.equal(message.get('uiCreated'), '1 year ago')
+  assert.strictEqual(message.get('uiCreated'), '1 year ago')
 })
 
 test('Age for message created 2 year (incl) or more years ago', function (assert) {
-  const eta = 7
+  const years = 7
   const created = new Date()
-  created.setFullYear(created.getFullYear() - eta)
+  created.setFullYear(created.getFullYear() - years)
   let message = this.subject({ created: created })
-  assert.equal(message.get('uiCreated'), `${eta} years ago`)
+  assert.strictEqual(message.get('uiCreated'), `${years} years ago`)
 })
--- tests/unit/models/user-test.js
+++ tests/unit/models/user-test.js
@@ -13,11 +13,11 @@ moduleForModel('user', 'Unit | Model | user', {
 
 test('Image for a user who does not have a image', function (assert) {
   let model = this.subject({ image: null })
-  assert.equal(model.get('uiImage'), 'no-user-image.png')
+  assert.strictEqual(model.get('uiImage'), 'no-user-image.png')
 })
 
 test('Image for a user who does have a image', function (assert) {
   const userImage = 'user.png'
   let model = this.subject({ image: userImage })
-  assert.equal(model.get('uiImage'), userImage)
+  assert.strictEqual(model.get('uiImage'), userImage)
 })
