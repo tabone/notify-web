3c69e119455fcb1d2526c8950986572ffb536f5e
Luca Tabone <taboneluca3@gmail.com>
2017-04-10 20:52:52 +0200

    Remove redundant new line

--- tests/unit/routes/chat/index-test.js
+++ tests/unit/routes/chat/index-test.js
@@ -1,9 +1,6 @@
 import { moduleFor, test } from 'ember-qunit'
 
-moduleFor('route:chat/index', 'Unit | Route | chat/index', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
-})
+moduleFor('route:chat/index', 'Unit | Route | chat/index', {})
 
 test('it exists', function (assert) {
   let route = this.subject()
