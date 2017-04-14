0b6a01e56399ff38b0142156007d9a9defebb0b3
Luca Tabone <taboneluca3@gmail.com>
2017-04-14 14:03:48 +0200

    Include number of expected assertions

--- tests/unit/instance-initializers/ajax-setup-test.js
+++ tests/unit/instance-initializers/ajax-setup-test.js
@@ -21,6 +21,7 @@ module('Unit | Instance Initializer | ajax setup', {
 // When making a AJAX Request, it should by default include the
 // 'withCredentials' header set to 'true'.
 test('it works', function (assert) {
+  assert.expect(2)
   // Initialize app.
   initialize(this.appInstance)
 
