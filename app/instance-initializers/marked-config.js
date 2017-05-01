a112a14095b39ad0a8a9fec5aac4e3fdc67455ef
Luca Tabone <tabone@users.noreply.github.com>
2017-05-01 13:28:25 +0200

    Add instance initializer tests (#18)

--- app/instance-initializers/marked-config.js
+++ app/instance-initializers/marked-config.js
@@ -1,37 +1,47 @@
 /**
  * Instance initializer used to setup the markdown library used for chat
  * messages.
+ * Libraries:
+ *   > Marked: https://github.com/chjj/marked
+ *   > HighlightJS: https://github.com/isagalaev/highlight.js
  */
 export function initialize () {
-  // Configure marked library
+  // Configure `marked` library.
   marked.setOptions({
     sanitize: true,
     renderer: getRenderer()
   })
 
   /**
-   * getRenderer overrides the in built renderer.
+   * getRenderer overrides the `marked` built-in renderer.
    * @return {Object} Markdown renderer object.
    */
   function getRenderer () {
+    // Create a new Renderer instance.
     const renderer = new marked.Renderer()
 
-    // Disable header tags.
-    renderer.heading = (text, level) => {
-      return text
-    }
+    /**
+     * Customize the Renderer to not use heading tags.
+     * @param  {String} text  Title text.
+     * @return {String}       String to be used in the DOM.
+     */
+    renderer.heading = (text) => text
 
-    // Highlight code syntax.
+    /**
+     * Customize the Renderer to highlight the code using hljs library.
+     * @param  {String} code     Code to be highlighted.
+     * @param  {String} language Language of the code to be highlighted.
+     * @return {String}          String to be used in the DOM.
+     */
     renderer.code = (code, language) => {
-      code = (language === undefined)
-        // If no language is provided, use the automated feature.
-        ? hljs.highlightAuto(code).value
-        // Else highlight based on the language specified.
+      // Highlight code.
+      code = (language == undefined) ? hljs.highlightAuto(code).value
         : hljs.highlight(language, code).value
 
-      return `<pre><code class="hljs lang-${language}">${code}</code></pre>`
+      return `<pre><code class="hljs">${code}</code></pre>`
     }
 
+    // Return customized renderer.
     return renderer
   }
 }
--- tests/unit/instance-initializers/ajax-setup-test.js
+++ tests/unit/instance-initializers/ajax-setup-test.js
@@ -2,6 +2,8 @@ import Ember from 'ember'
 import { initialize } from 'notify/instance-initializers/ajax-setup'
 import { module, test } from 'qunit'
 import destroyApp from '../../helpers/destroy-app'
+import sinon from 'sinon'
+import $ from 'jquery'
 
 module('Unit | Instance Initializer | ajax setup', {
   beforeEach () {
@@ -16,10 +18,30 @@ module('Unit | Instance Initializer | ajax setup', {
   }
 })
 
-// Replace this with your real tests.
-test('it works', function (assert) {
+// When making a AJAX Request, it should by default include the
+// 'withCredentials' header set to 'true'.
+test('Doing an AJAX Request', function (assert) {
+  assert.expect(2)
+  // Initialize app.
   initialize(this.appInstance)
 
-  // you would normally confirm the results of the initializer here
-  assert.ok(true)
+  // Array to contain the XHR Requests done.
+  const requests = []
+  // Mock XHR Request.
+  const xhr = sinon.useFakeXMLHttpRequest()
+  // Store any requests done by the login() method.
+  xhr.onCreate = (request) => requests.push(request)
+
+  // Make an AJAX Request.
+  $.ajax({
+    url: 'http://hikupz.io/api',
+    method: 'POST'
+  })
+
+  // Verify that one request has been done.
+  assert.strictEqual(requests.length, 1)
+
+  // Verify that the request was done with the header 'withCredentials' set to
+  // 'true'
+  assert.ok(requests[0].withCredentials === true)
 })
